93390339c642c9a2239ad5a0f2599d18b6865551
Gerald Tee <93geraldtee@gmail.com>
2019-09-17 15:55:28 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,7 +6,9 @@ def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-
+def introduction_with_language(name, language, language = )
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
 
 # Write your code here. If you get stuck, at the very bottom of this lab we've included
